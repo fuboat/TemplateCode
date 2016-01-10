1956ae576cfabbf3c02ebe4b1efcfb286943d909
fuboat <fuboat@outlook.com>
2016-01-10 12:05:06 +0800

    Graph

--- Graph/无向图的割顶与桥.cpp
+++ Graph/无向图的割顶与桥.cpp
@@ -1,10 +1,43 @@
-template <typename T> bool Chkmax(int &x, int y){ return x < y? x = y, true : false; }
-template <typename T> bool Chkmin(int &x, int y){ return x > y? x = y, true : false; }
+template <typename T> bool Chkmax(T &x, T y){ return x < y? x = y, true : false; }
+template <typename T> bool Chkmin(T &x, T y){ return x > y? x = y, true : false; }
 
 const int MAXN = 20000, MAXM = 400000;
 int head[MAXN], to[MAXN], next[MAXN];
 
 bool iscut[MAXN], bccid[MAXN];
 struct Tarjan{
+	int low[MAXN], dfn[MAXN];
+	int dfs_clock;
 	
+	void Dfs(int u, int fa)
+	{
+		low[u] = dfn[u] = ++ dfs_clock;
+		
+		int v, child = 0;
+		for(int i = head[u];i; i = next[i]){
+			v = to[i];
+			if(!dfn[v]){
+				child ++;
+				Dfs(v, u);
+				Chkmin(low[u], low[v]);
+				if(low[v] < dfn[u])
+					iscut[u] = true;
+			}else if(dfn[v] < dfn[u] && v != fa)
+				Chkmin(low[u], dfn[v]);
+		}
+		
+		if(fa == -1 && child < 2)
+			iscut[u] = false;
+	}
+	
+	void Exec(int N){
+		CLEAR(low);
+		CLEAR(dfn);
+		CLEAR(iscut);
+		dfs_clock = 0;
+		
+		REP(i, 1, N)
+			if(!dfn[i])
+				Dfs(i, -1);
+	}
 };
--- Graph/点-双连通分量.cpp
+++ Graph/点-双连通分量.cpp
@@ -1,15 +1,61 @@
-#define pii pair<int, int>
-template <typename T> bool Chkmax(int &x, int y){ return x < y? x = y, true : false; }
-template <typename T> bool Chkmin(int &x, int y){ return x > y? x = y, true : false; }
+#include <vector>
+#include <stack>
+#include <iostream>
+#define Pii pair<int, int>
+using namespace std;
+#define X first
+#define Y second
+#define MK(a, b) make_pair(a, b)
+template <typename T> bool Chkmax(T &x, T y){ return x < y? x = y, true : false; }
+template <typename T> bool Chkmin(T &x, T y){ return x > y? x = y, true : false; }
 
 const int MAXN = 20000, MAXM = 400000;
 int head[MAXN], to[MAXN], next[MAXN];
 
-bool iscut[MAXN], bccid[MAXN];
+bool iscut[MAXN];
+int bccid[MAXN];
+vector<int> bcc[MAXN * 2];
+int totbcc;
 struct Tarjan{
-	pii st[MAXM];
-	int top;
-	int pre[MAXN];
+	stack<Pii>st;
+	int dfn[MAXN], low[MAXN];
+	int dfs_clock;
 
-	bool Dfs(int u){}
+	bool Dfs(int u, int fa)
+	{
+		int v, child(0);
+		Pii E, S;
+		low[u] = dfn[u] = ++ dfs_clock;
+		
+		for(int i = head[u]; i; i = next[i]){
+			E = MK(u, v);
+			v = to[i];
+			if(!dfn[v]){
+				child ++;
+				st.push(E);
+				Dfs(v, u);
+				Chkmin(low[u], low[v]);
+				if(low[v] < dfn[u]){
+					totbcc ++;
+					iscut[u] = true;
+					while(!st.empty()){
+						S = st.top();
+						st.pop();
+						if(Chkmax(bccid[S.X], totbcc))
+							bcc[totbcc].push_back(S.X);
+						if(Chkmax(bccid[S.Y], totbcc))
+							bcc[totbcc].push_back(S.Y);
+						if(S == E) break;
+					}
+				}
+			}else if(dfn[v] < dfn[u] && u != fa){
+				st.push(E);
+				// Don't forget!
+				Chkmin(low[u], dfn[v]);
+			}
+			
+			if(fa == -1 && child < 2)
+				iscut[u] = false;
+		}
+	}
 };
